364a30b9328952b2c9154613dcc93dc6cd8f1dbb
donny <donnyardiansyah@polije.ac.id>
2024-03-13 21:04:28 +0700

    ui lupa password
    no fix

--- lib/register/lupapassword_page.dart
+++ lib/register/lupapassword_page.dart
@@ -4,10 +4,97 @@ class LupaPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('reset password'),
-      ),
-      body: Center(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey,
+            ),
+            child: Image.asset(
+              'assets/images/logo_bem_polije.jpg',
+              width: 40,
+              height: 40,
+              fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "Login",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(40, 20, 40, 5),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(40, 5, 40, 5),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Password",
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      });
+                    }),
+              ),
+            ),
+          ),
+          ]
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LupaPassword()),
+                    );
+                  },
+                  child: Text(
+                    "Lupa password?",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black87,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(40, 40, 40, 5),
+            height: 100,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF5F7C5D),
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              child: Text(
+                "Login",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
     );
   }
 }
